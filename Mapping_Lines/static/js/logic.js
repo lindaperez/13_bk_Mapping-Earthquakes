029ac7e855f7c857428374d190add00c96a0e15d
Linda Perez <rosmery.p.p@gmail.com>
2022-04-26 16:22:50 -0700

    map shows line between two points

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,27 @@
+console.log("Working...")
+// Create the map object with a center and zoom level.
+
+var map = L.map('mapid').setView([36.1733, -120.1794], 7);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+    tileSize: 512,
+    zoomOffset: -1,
+    id: 'mapbox/streets-v11'
+
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "red"
+  }).addTo(map);
