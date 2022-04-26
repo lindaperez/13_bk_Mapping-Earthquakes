21dfcc5c820d2faae84b11733059e5814a9c6512
Linda Perez <rosmery.p.p@gmail.com>
2022-04-26 16:38:44 -0700

    map with style satellite street show some lines from Seattle to LA

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,7 +1,7 @@
 console.log("Working...")
 // Create the map object with a center and zoom level.
 
-var map = L.map('mapid').setView([36.1733, -120.1794], 7);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -10,18 +10,21 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     accessToken: API_KEY,
     tileSize: 512,
     zoomOffset: -1,
-    id: 'mapbox/streets-v11'
+    id: 'mapbox/satellite-streets-v11'
 
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-// Coordinates for each point to be used in the line.
+
+// Coordinates for each point to be used in the polyline.
 let line = [
     [33.9416, -118.4085],
-    [37.6213, -122.3790]
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
   ];
-
 // Create a polyline using the line coordinates and make the line red.
+
 L.polyline(line, {
-    color: "red"
-  }).addTo(map);
+    color: "yellow"
+ }).addTo(map);
