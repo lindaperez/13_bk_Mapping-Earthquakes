5ebda1d28aca9a833e5faa2edf0bc5e9cee22ee2
Linda Perez <rosmery.p.p@gmail.com>
2022-04-28 13:08:50 -0700

    styling + html with leaflet 1.8

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,9 +1,10 @@
 // Center of the map
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
+var map = L.map('mapid').setView([37.5, -122.5], 10);
+const atributesTileLayer = 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>';
+const tileLayerUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}';
 //streets tile layer
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+let streets = L.tileLayer(tileLayerUrl, {
+    attribution: atributesTileLayer,
     maxZoom: 18,
     accessToken: API_KEY,
     tileSize: 512,
@@ -12,8 +13,8 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 
 });
 // dark layer
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+let dark = L.tileLayer(tileLayerUrl, {
+    attribution: atributesTileLayer,
     maxZoom: 18,
     accessToken: API_KEY,
     tileSize: 512,
@@ -51,9 +52,11 @@ L.geoJSON(sanFanAirport, {
     pointToLayer: function (feature, latlng) {
         console.log('Feature',feature)
         let geoMarker =  L.marker(latlng)
-        .bindPopup("<h3>"+feature.properties.name+"</h3>\
-        <hr> <b>"+feature.properties.city+", "
-        +feature.properties.country+"</b>");
+        .bindPopup("<h3> Airport code: "+
+        feature.properties.faa+
+        "</h3><hr><b>Airport Name: "+
+        feature.properties.name+
+        "</b>");
         sfoAirport.push(geoMarker);
         return geoMarker
     }
