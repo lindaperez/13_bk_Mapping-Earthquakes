009d5aecad1db9def4c0fe486350070a477a6678
Linda Perez <rosmery.p.p@gmail.com>
2022-04-28 11:50:01 -0700

    no changes perceive end of Mapping_Lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,7 +1,7 @@
 console.log("Working...")
 // Create the map object with a center and zoom level.
 
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([30.1926,-97.6650], 5);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -18,13 +18,16 @@ streets.addTo(map);
 
 // Coordinates for each point to be used in the polyline.
 let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
+    [37.6212,-122.3829], //SFO
+    [30.1926,-97.6650], //AUS
+    [43.6809,-79.6136], //YYZ
+    [40.6416,-73.7797],  //JFK
+    [37.3622,-121.9294] //SJC
   ];
 // Create a polyline using the line coordinates and make the line red.
 
 L.polyline(line, {
-    color: "yellow"
+    color: "blue",
+    weight: 4,
+    opacity: 0.5
  }).addTo(map);
